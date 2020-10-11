45d19be69038535de48ae4b4c22e024a86ece7c2
simon <simonzgx@gmail.com>
2020-10-11 19:17:52 +0800

    add sleep command

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+//
+// Created by Simon on 2020/10/11.
+//
+
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(2, "usage: sleep [n] seconds\n");
+        exit(1);
+    }
+
+    int n = atoi(argv[1]);
+    sleep(n);
+    exit(0);
+}
