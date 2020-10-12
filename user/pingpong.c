4120fcd44e90ca65a8e85d160916564a3a89ded8
Simon <simonzgx@gmail.com>
2020-10-12 18:20:52 +0800

    add pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -8,24 +8,33 @@
 
 int main(int argc, char *argv[]) {
 
-    int p[2];
-    pipe(p);
+    int p[2][2];
+    pipe(p[0]);
+    pipe(p[1]);
     char val = 1;
     if (fork() == 0) {
+        //child process
         int pid = getpid();
-        while (read(p[1], &val, sizeof val) <= 0);
+        close(p[0][1]);
+        close(p[1][0]);
+        read(p[0][0], &val, sizeof val);
+        write(p[1][1], &val, sizeof val);
+        close(p[1][1]);
+        close(p[0][0]);
         printf("%d:received ping\n", pid);
         exit();
     } else {
+        //parent process
         int pid = getpid();
-        while (write(p[0], &val, sizeof val) <= 0);
-        while (read(p[1], &val, sizeof val) <= 0);
+        close(p[0][0]);
+        close(p[1][1]);
+        write(p[0][1], &val, sizeof val);
+        read(p[1][0], &val, sizeof val);
         printf("%d:received pong\n", pid);
+        close(p[1][0]);
+        close(p[0][1]);
         wait();
     }
-
-    close(p[0]);
-    close(p[1]);
 
     exit();
 }
